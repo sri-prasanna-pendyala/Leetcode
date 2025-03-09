1c072452f2dd471b991bb5dfb1cebbb6fc3ec9af
Sri Prasanna <155663635+sri-prasanna-pendyala@users.noreply.github.com>
2025-03-09 21:07:06 +0530

    Time: 0 ms (100%), Space: 28.1 MB (84.36%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,14 +1,33 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int,int> mp; //unordered map uses O(1) for insertion
-        int val=nums.size()/2; 
-        for(int i:nums) //iterating through nums
+        // unordered_map<int,int> mp; //unordered map uses O(1) for insertion
+        // int val=nums.size()/2; 
+        // for(int i:nums) //iterating through nums
+        // {
+        //     mp[i]++; //mp[0..n]: val++ ---> mp[i]++
+        //     if(mp[i]>val) //early checking if freq> n/2 like mp[0] returns value of that key
+        //         return i;
+        // }
+        // return -1;
+
+        //Boyer-Moore Voting Algorithm for O(1) space and O(n) time 
+        int ele,count=0; //ele for storing majority element and count for counting occurences
+        for(int i:nums)
         {
-            mp[i]++; //mp[0..n]: val++ ---> mp[i]++
-            if(mp[i]>val) //early checking if freq> n/2 like mp[0] returns value of that key
-                return i;
+            if(count==0)
+            {
+                ele=i;
+                //as the ele occurs, the count will be incremented to 1 at first
+                count=1;
+            }
+            else if(ele==i)
+            //if the ele appeared twice the count will be incremented
+                count++;
+            else
+                count--;
         }
-        return -1;
+        // As it is sure that there will be majority ele in the array
+        return ele;
     }
 };
