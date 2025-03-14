4def1d7785f2fb69c5274316b7396026a2cdf6b9
Sri Prasanna <155663635+sri-prasanna-pendyala@users.noreply.github.com>
2025-03-15 00:34:36 +0530

    Time: 0 ms (100%), Space: 22.5 MB (96.57%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int move=1,uniq=nums[0],dup=-1;
+        for(int i=0;i<nums.size();i++)
+        {
+            //if uniq num is found in array...its is duplicated one..so dup++
+            if(uniq==nums[i])
+            {
+                dup++;
+                continue;
+            }
+            else
+            {
+                //else we set the newly found num as uniq num found so far..and swap the
+                //uniq num with the place where the nxt uniq num should go
+                uniq=nums[i];
+                swap(nums[move],nums[i]);
+                //we increment the place to nxt one
+                move++;
+            }
+        }
+        return nums.size()-dup;
+    }
+
+};
