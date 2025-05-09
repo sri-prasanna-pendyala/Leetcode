83fa66d0fc52b5ef00009519b2a0fa6414e87f39
Sri Prasanna <155663635+sri-prasanna-pendyala@users.noreply.github.com>
2025-05-09 22:40:59 +0530

    Time: 0 ms (100%), Space: 22.8 MB (19.29%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,26 +1,39 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int move=1,uniq=nums[0],dup=-1;
-        for(int i=0;i<nums.size();i++)
+        int key=0,place=1,temp=0;
+        for(int i=1;i<nums.size();i++)
         {
-            //if uniq num is found in array...its is duplicated one..so dup++
-            if(uniq==nums[i])
+            if(nums[i]!=nums[key])
             {
-                dup++;
-                continue;
-            }
-            else
-            {
-                //else we set the newly found num as uniq num found so far..and swap the
-                //uniq num with the place where the nxt uniq num should go
-                uniq=nums[i];
-                swap(nums[move],nums[i]);
-                //we increment the place to nxt one
-                move++;
+                temp=nums[i];
+                nums[i]=nums[place];
+                nums[place]=temp;
+                key++;
+                place++;
             }
         }
-        return nums.size()-dup;
+        return place;
+        // int move=1,uniq=nums[0],dup=-1;
+        // for(int i=0;i<nums.size();i++)
+        // {
+        //     //if uniq num is found in array...its is duplicated one..so dup++
+        //     if(uniq==nums[i])
+        //     {
+        //         dup++;
+        //         continue;
+        //     }
+        //     else
+        //     {
+        //         //else we set the newly found num as uniq num found so far..and swap the
+        //         //uniq num with the place where the nxt uniq num should go
+        //         uniq=nums[i];
+        //         swap(nums[move],nums[i]);
+        //         //we increment the place to nxt one
+        //         move++;
+        //     }
+        // }
+        // return nums.size()-dup;
     }
 
 };
