49a3b7d174b31f746b55d9fd15432f9b1f822544
Sri Prasanna <155663635+sri-prasanna-pendyala@users.noreply.github.com>
2025-05-12 12:24:25 +0530

    Time: 0 ms (100%), Space: 23.8 MB (78.15%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int nonzero=0;
-        //variable nonzero stores the place where the nxt nonzero index have to swap with.
+        int zero=0,temp=0;
         for(int i=0;i<nums.size();i++)
         {
-            //if the element is nonzero value then swap with the variable nonzero.This moves the nonzero's to the front by pushing the zeroes to the every non zero index which are encountered.
             if(nums[i]!=0)
             {
-                swap(nums[i],nums[nonzero]);
-                //after swapping the elements we increment the nonzero index if there are adjacent zeroes
-                nonzero++;
+                temp=nums[i];
+                nums[i]=nums[zero];
+                nums[zero]=temp;
+                zero++;
             }
-        }  
+        }
+        // int nonzero=0;
+        // for(int i=0;i<nums.size();i++)
+        // {
+        //     if(nums[i]!=0)
+        //     {
+        //         swap(nums[i],nums[nonzero]);
+        //         nonzero++;
+        //     }
+        // }  
     }
 };
