d5d3ba284313783521e1dbc884e4e79894e70cf9
Sri Prasanna <155663635+sri-prasanna-pendyala@users.noreply.github.com>
2025-11-26 16:11:58 +0530

    Time: 4 ms (62.96%), Space: 13.3 MB (80.47%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
-        vector<int> v;
-        //for index i we need this as the nxt element in the array
-        int index=(2*n)/2;
-        for(int i=0;i<n;i++)
-        {
+        vector<int> ans;
+        for(int i=0;i<n;i++){
             //we iterate through halfway of array & assign w.r.to the same index
-            v.push_back(nums[i]);
-            v.push_back(nums[i+index]);
+            ans.push_back(nums[i]);
+            ans.push_back(nums[i+n]);
         }
-        return v;
+        return ans;
     }
 };
