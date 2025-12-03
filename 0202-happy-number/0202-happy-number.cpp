b8f31e32779a46c503cbe966bd5b3aeb3da92343
Sri Prasanna <155663635+sri-prasanna-pendyala@users.noreply.github.com>
2025-12-03 19:38:20 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-    bool isHappy(int n) {
-        int sum=0,rem=0;
-        while(n>0)
-        {
-            rem=n%10;
-            sum+=pow(rem,2);
-            n=n/10;
-            if(n==0)
-            {
-                if(sum>=10)
-                {
-                    n=sum;
-                    sum=0;
-                }
-                else if(sum>1)
-                    return false;
-            }
+    int squaresum(int a){
+        int res=0;
+        while(a>0){
+            res+=(a%10)*(a%10);
+            a/=10;
         }
-        return true;
+        return res;
+    }
+    bool isHappy(int n) {
+        int sum;
+        if(n<9)
+            return (n==1)?true:false;
+        while(n>1){
+            sum=squaresum(n);
+            if(sum==1)
+                return true;
+            else
+                n=sum;
+        }
+        return (n==1)?true:false;
     }
 };
