d58b8ae6905dd4312c5d38aec8d04d82d35754a4
Sri Prasanna <155663635+sri-prasanna-pendyala@users.noreply.github.com>
2025-12-05 21:36:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        //bruteforce
-        if(m==0){
-            nums1=nums2;
-            return;
+        if(m==0)
+            nums1[0]=nums2[0];
+        int a=m-1,b=m+n-1,c=n-1,temp;
+        while(a>=0 && c>=0){
+            if(nums1[a]>nums1[b] && nums1[a]>=nums2[c]){
+                temp=nums1[a];
+                nums1[a]=nums1[b];
+                nums1[b]=temp;
+                b--;
+                a--;
+            }
+            else if(nums2[c]>nums1[b]){
+                temp=nums2[c];
+                nums2[c]=nums1[b];
+                nums1[b]=temp;
+                c--;
+                b--;
+            }
         }
-        else if(n==0)
-            return;
-        int k=0;
-        for(int i=nums1.size()-1;i>=m;i--){
-            nums1[i]=nums2[--n];
-        }
-        sort(nums1.begin(),nums1.end());
+        
     }
 };
