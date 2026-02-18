b1c8880ac0288db8c4ec50f27baa057491e2dc86
Sri Prasanna <24a95a0502@aec.edu.in>
2026-02-18 16:45:48 +0530

    Time: 0 ms (100%), Space: 23.9 MB (19.53%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,17 +1,28 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int zero=0,temp=0;
-        for(int i=0;i<nums.size();i++)
-        {
-            if(nums[i]!=0)
-            {
-                temp=nums[i];
-                nums[i]=nums[zero];
-                nums[zero]=temp;
-                zero++;
+        //feb 2026
+        int l=0,r=0;
+        while(r<nums.size()){
+            if(nums[r]!=0){
+                swap(nums[l],nums[r]);
+                l++;
             }
+            r++;
         }
+        //may 2025
+        // int zero=0,temp=0;
+        // for(int i=0;i<nums.size();i++)
+        // {
+        //     if(nums[i]!=0)
+        //     {
+        //         temp=nums[i];
+        //         nums[i]=nums[zero];
+        //         nums[zero]=temp;
+        //         zero++;
+        //     }
+        // }
+        // march 2025 
         // int nonzero=0;
         // for(int i=0;i<nums.size();i++)
         // {
