664fb5555e085c9b20d65bed85912b8f0a347488
Sri Prasanna <24a95a0502@aec.edu.in>
2026-02-18 16:01:20 +0530

    Time: 0 ms (100%), Space: 22.5 MB (97.22%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,39 +1,16 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int key=0,place=1,temp=0;
-        for(int i=1;i<nums.size();i++)
-        {
-            if(nums[i]!=nums[key])
-            {
-                temp=nums[i];
-                nums[i]=nums[place];
-                nums[place]=temp;
-                key++;
-                place++;
+        int l=0,r=0,k=1;
+        while(r<nums.size()){
+            if(nums[l]==nums[r])
+                r++;
+            else{
+                swap(nums[++l],nums[r++]);
+                k++;
             }
         }
-        return place;
-        // int move=1,uniq=nums[0],dup=-1;
-        // for(int i=0;i<nums.size();i++)
-        // {
-        //     //if uniq num is found in array...its is duplicated one..so dup++
-        //     if(uniq==nums[i])
-        //     {
-        //         dup++;
-        //         continue;
-        //     }
-        //     else
-        //     {
-        //         //else we set the newly found num as uniq num found so far..and swap the
-        //         //uniq num with the place where the nxt uniq num should go
-        //         uniq=nums[i];
-        //         swap(nums[move],nums[i]);
-        //         //we increment the place to nxt one
-        //         move++;
-        //     }
-        // }
-        // return nums.size()-dup;
+        return k;
     }
 
 };
