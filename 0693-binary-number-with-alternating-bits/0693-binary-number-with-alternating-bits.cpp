0e5fd2c52898e0808d71252efe0404ebb0097eca
Sri Prasanna <24a95a0502@aec.edu.in>
2026-02-18 09:11:48 +0530

    Time: 0 ms (100%), Space: 7.5 MB (99.91%) - LeetHub

--- 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.cpp
+++ 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.cpp
@@ -1,20 +1,35 @@
 class Solution {
 public:
     bool hasAlternatingBits(int n) {
-        string ans="";
-        while(n>0){
-            int rem=n%2;
-            ans.push_back('0'+rem);
-            n/=2;
-        }
-        reverse(ans.begin(),ans.end());
-        int ele=ans[0];
-        for(int i=1;i<ans.size();i++){
-            if(ans[i]==ele)
-                return false;
-            else
-                ele=ans[i];
-        }
-        return true;
+        // Time : O(logn) space: O(logn)
+        // string ans="";
+        // while(n>0){
+        //     int rem=n%2;
+        //     ans.push_back('0'+rem);
+        //     n/=2;
+        // }
+        // reverse(ans.begin(),ans.end());
+        // int ele=ans[0];
+        // for(int i=1;i<ans.size();i++){
+        //     if(ans[i]==ele)
+        //         return false;
+        //     else
+        //         ele=ans[i];
+        // }
+        // return true;
+
+        //optimsed space complexity with O(1)
+        // int rem=n%2;
+        // n/=2;
+        // while(n>0){
+        //     if(rem==n%2)    return false;
+        //     rem=n%2;
+        //     n/=2;
+        // }
+        // return true;
+
+        //optimsed approach with time & space O(1) using bit manipulation
+        unsigned int bits=(n^(n>>1));
+        return (bits&(bits+1))==0;
     }
 };
