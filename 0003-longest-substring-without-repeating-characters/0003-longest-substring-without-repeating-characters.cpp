e8fe1a6b34e61cfe27507e9eb5927e89610a2fb3
Sri Prasanna <24a95a0502@aec.edu.in>
2026-02-19 23:52:56 +0530

    Time: 3 ms (79.54%), Space: 11.7 MB (63.94%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,33 +1,46 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        if(s.size()==0)
-            return 0;
-        else if(s.size()<2)
-            return 1;
-        vector<int> vec;
+        // optimised sol with O(N) TC but with O(256) space
         int maxlen=0;
-        //brute force logic with O(n^2) TC
-        for(int i=0;i<s.size();i++){
-            if(count(vec.begin(),vec.end(),s[i])){
-                for(int j=0;j<vec.size();j++){
-                    if(vec[j]==s[i]){
-                        vec.erase(vec.begin()+j);
-                        vec.push_back(s[i]);
-                        break;
-                    }
-                    else{
-                        vec.erase(vec.begin());
-                        j--;
-                    }
-                }
+        unordered_map<char,int> mp;
+        for(char c:s)mp[c]=-1;
+        int l=0,r=0;
+        while(r<s.size()){
+            if(mp[s[r]]!=-1){
+                if(mp[s[r]]>=l) // check whether found char is within window or not
+                    l=mp[s[r]]+1;
             }
-            else{
-                vec.push_back(s[i]);
-                maxlen=max(maxlen,(int)vec.size());
-            }
+            mp[s[r]]=r;
+            maxlen=max(maxlen,r-l+1);
+            r++;
         }
-        maxlen=max(maxlen,(int)vec.size());
+
+        //brute force logic with O(n^2) TC
+        // if(s.size()==0)
+        //     return 0;
+        // else if(s.size()<2)
+        //     return 1;
+        // for(int i=0;i<s.size();i++){
+        //     if(count(vec.begin(),vec.end(),s[i])){
+        //         for(int j=0;j<vec.size();j++){
+        //             if(vec[j]==s[i]){
+        //                 vec.erase(vec.begin()+j);
+        //                 vec.push_back(s[i]);
+        //                 break;
+        //             }
+        //             else{
+        //                 vec.erase(vec.begin());
+        //                 j--;
+        //             }
+        //         }
+        //     }
+        //     else{
+        //         vec.push_back(s[i]);
+        //         maxlen=max(maxlen,(int)vec.size());
+        //     }
+        // }
+        // maxlen=max(maxlen,(int)vec.size());
         return maxlen;
     }
 };
