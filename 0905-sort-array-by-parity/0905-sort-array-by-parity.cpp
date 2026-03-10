a5b0ef8f78caf07d65b5469db4164ac9524ecd75
Sri Prasanna <24a95a0502@aec.edu.in>
2026-03-10 22:42:47 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
@@ -1,16 +1,29 @@
 class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
-        //brute force approach
-        vector<int> res;
-        for(int i:nums){
-            if(i%2==0)
-                res.push_back(i);
+        //brute force approach bcoz space O(n)
+        // vector<int> res;
+        // for(int i:nums){
+        //     if(i%2==0)
+        //         res.push_back(i);
+        // }
+        // for(int i:nums){
+        //     if(i%2==1)
+        //         res.push_back(i);
+        // }
+        // return res;
+        
+        //using 2 pointer approach with O(1) space
+        int i=0,j=0,n=nums.size();
+        while(i<n && j<n){
+            if(nums[j]%2==1)
+                j++;
+            else if(nums[i]%2==0)
+                i++;
+            else{
+                swap(nums[i++],nums[j++]);
+            }
         }
-        for(int i:nums){
-            if(i%2==1)
-                res.push_back(i);
-        }
-        return res;
+        return nums;
     }
 };
