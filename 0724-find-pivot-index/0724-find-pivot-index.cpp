1c011d02046da32a15c76322401fb6b9f6cd943e
Sri Prasanna <24a95a0502@aec.edu.in>
2026-03-28 18:03:12 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -1,43 +1,17 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
-        int n=nums.size();
-        vector<int> leftsum(n),rightsum(n);
-        leftsum[0]=nums[0];
-        for(int i=1;i<n;i++){
-            leftsum[i]=nums[i]+leftsum[i-1];
+        int presum=0;
+        for(int i:nums){
+            presum+=i;
         }
-        rightsum[0]=nums[n-1];
-        for(int i=1;i<n;i++){
-            rightsum[i]=nums[n-i-1]+rightsum[i-1];
-        }
-        int k=n-1;
-        for(int i=0;i<n;i++){
-            if(leftsum[i]-rightsum[k]==0)
+        int sum=0;
+        for(int i=0;i<nums.size();i++){
+            presum-=nums[i];
+            if(presum==sum)
                 return i;
-            k--;
+            sum+=nums[i];
         }
         return -1;
-
-
-        //One of the approach
-        // int n=nums.size();
-        // vector<int> sumleft(n,0);
-        // int runsum=0;
-        // for(int i=0;i<n;i++){
-        //     sumleft[i]=runsum;
-        //     runsum+=nums[i];
-        // }
-        // runsum=0;
-        // vector<int> sumright(n,0);
-        // for(int i=n-1;i>=0;i--){
-        //     sumright[i]=runsum;
-        //     runsum+=nums[i];
-        // }
-        // for(int i=0;i<n;i++){
-        //     if(sumleft[i]==sumright[i])
-        //         return i;
-        // }
-        // return -1;
     }
 };
