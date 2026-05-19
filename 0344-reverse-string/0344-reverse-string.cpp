6a392f52ae0deb41f82045b6581fdb85a8422fa2
Sri Prasanna <24a95a0502@aec.edu.in>
2026-05-19 13:53:05 +0530

    Time: 0 ms (100%), Space: 27.2 MB (50.95%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,12 +1,9 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        int n=s.size()/2;
-        int i=0,j=s.size()-1;
-        while(i<n){
-            char temp=s[i];
-            s[i++]=s[j];
-            s[j--]=temp;
+        int l=0,r=s.size()-1;
+        while(l<r){
+            swap(s[l++],s[r--]);
         }
     }
 };
