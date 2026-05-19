3d819e64232c393f12a77b4b07338ef33b4bee49
Sri Prasanna <24a95a0502@aec.edu.in>
2026-05-19 15:14:13 +0530

    Time: 0 ms (100%), Space: 19.4 MB (63.67%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        // initialise 2 pointers -- one from start & one from end
-        int i=0,j=numbers.size()-1;
-        while(j<numbers.size()){
-            // if sum == target return indexes+1
-            if(numbers[i]+numbers[j]==target)
-                return {i+1,j+1};
-            //if sum<target we have to search in upper part of array as it is ascend order
-            else if(numbers[i]+numbers[j]<target)
-                i++;
-            // else then search lower part of array like (-10+16=6 for target 4 so search lower part by decrementing from 6 to 2)
+        int l=0,r=numbers.size()-1;
+        while(l<r){
+            if(numbers[l]+numbers[r]==target)
+                return {l+1,r+1};
+            else if(numbers[l]+numbers[r]>target)
+                r--;
             else
-                j--;
-            
+                l++;
         }
-        return {-1,-1};
+        return {l+1,r+1};
     }
 };
