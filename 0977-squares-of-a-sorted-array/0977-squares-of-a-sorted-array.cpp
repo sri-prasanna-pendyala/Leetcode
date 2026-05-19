0ef744bb21bbc1e3a96e3f32b096602fd7b4fbb2
Sri Prasanna <24a95a0502@aec.edu.in>
2026-05-19 14:27:17 +0530

    Time: 0 ms (100%), Space: 31.4 MB (12.17%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -1,24 +1,37 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        int l=0,r=nums.size()-1,temp;
-        while(l<=r && r>0){
-            if(abs(nums[l])>abs(nums[r])){
-                temp=nums[l];
-                nums[l]=nums[r];
-                nums[r]=temp;      
-            }
-            r--;
-        }
-        for(int i=0;i<nums.size();i++){
+        int n=nums.size(), mini=INT_MAX, l, r;
+        vector<int> v;
+        for(int i=0;i<n;i++){
             nums[i]*=nums[i];
+            mini=min(mini,nums[i]);
+            if(mini==nums[i])
+                l=i;
         }
-        return nums;
-        //brute force
-        // for(int i=0;i<nums.size();i++){
-        //     nums[i]*=nums[i];
-        // }
-        // sort(nums.begin(),nums.end());
-        // return nums;
+        r=l+1;
+        if(l==0)
+            return nums;
+        else if(r>=n){
+            r=n-1;l=0;
+            while(l<r)
+                swap(nums[l++],nums[r--]);
+            return nums;
+        }
+        else{
+            r=l+1;
+            while(l>=0 && r<n){
+                if(nums[l]<nums[r]){
+                    v.push_back(nums[l--]);
+                }
+                else
+                    v.push_back(nums[r++]);
+            }
+            while(l>=0)
+                v.push_back(nums[l--]);
+            while(r<n)
+                v.push_back(nums[r++]);
+        }
+        return v;
     }
 };
