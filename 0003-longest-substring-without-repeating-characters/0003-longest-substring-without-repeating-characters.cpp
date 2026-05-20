b314a2aba15f3bbc390e8f2caa4e6e3f908cfec4
Sri Prasanna <24a95a0502@aec.edu.in>
2026-05-20 23:09:38 +0530

    Time: 14 ms (39.83%), Space: 11.8 MB (56.16%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,46 +1,19 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        // optimised sol with O(N) TC but with O(256) space
-        int maxlen=0;
-        unordered_map<char,int> mp;
-        for(char c:s)mp[c]=-1;
-        int l=0,r=0;
-        while(r<s.size()){
-            if(mp[s[r]]!=-1){
-                if(mp[s[r]]>=l) // check whether found char is within window or not
-                    l=mp[s[r]]+1;
+        unordered_map<char,int> seen;
+        int l=0, r=0, longest=0, n;
+        while(r<s.size() && l<=r){
+            if(seen.contains(s[r])){
+                n=r-l;
+                longest= max(longest, n);
+                if(seen[s[r]]>=l)
+                    l=seen[s[r]]+1;
             }
-            mp[s[r]]=r;
-            maxlen=max(maxlen,r-l+1);
+            seen[s[r]]=r;
             r++;
         }
-
-        //brute force logic with O(n^2) TC
-        // if(s.size()==0)
-        //     return 0;
-        // else if(s.size()<2)
-        //     return 1;
-        // for(int i=0;i<s.size();i++){
-        //     if(count(vec.begin(),vec.end(),s[i])){
-        //         for(int j=0;j<vec.size();j++){
-        //             if(vec[j]==s[i]){
-        //                 vec.erase(vec.begin()+j);
-        //                 vec.push_back(s[i]);
-        //                 break;
-        //             }
-        //             else{
-        //                 vec.erase(vec.begin());
-        //                 j--;
-        //             }
-        //         }
-        //     }
-        //     else{
-        //         vec.push_back(s[i]);
-        //         maxlen=max(maxlen,(int)vec.size());
-        //     }
-        // }
-        // maxlen=max(maxlen,(int)vec.size());
-        return maxlen;
+        longest= max(longest, r-l);
+        return (longest==INT_MIN)?1:longest;
     }
 };
