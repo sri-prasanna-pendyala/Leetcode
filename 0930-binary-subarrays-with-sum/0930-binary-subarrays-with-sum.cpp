b47366a5f78636d5bcc974906c22d5e5d41ee225
Sri Prasanna <24a95a0502@aec.edu.in>
2026-05-29 16:02:42 +0530

    Time: 19 ms (28.86%), Space: 38.8 MB (17.86%) - LeetHub

---
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        int sum=0,cnt=0;
+        unordered_map<int,int> mp;
+        mp[0]=1;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            if(mp.find(sum-goal)!=mp.end()){
+                cnt+=mp[sum-goal];
+            }
+            mp[sum]++;
+        }
+        return cnt;
+    }
+};
