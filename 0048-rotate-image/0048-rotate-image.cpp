4cc6cddf01a304758d8e0bb7d632241d6396358b
Sri Prasanna <24a95a0502@aec.edu.in>
2026-06-03 11:58:17 +0530

    Time: 0 ms (100%), Space: 10.1 MB (91.54%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -2,16 +2,12 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         for(int i=0;i<matrix.size();i++){
-            for(int j=i+1;j<matrix[i].size();j++){
+            for(int j=i;j<matrix[i].size();j++){
                 swap(matrix[i][j],matrix[j][i]);
             }
         }
         for(int i=0;i<matrix.size();i++){
-            int last=matrix[i].size()-1;
-            for(int j=0;j<matrix[i].size()/2;j++){
-                swap(matrix[i][j],matrix[i][last]);
-                last--;
-            }
+           reverse(matrix[i].begin(),matrix[i].end());
         }
     }
 };
