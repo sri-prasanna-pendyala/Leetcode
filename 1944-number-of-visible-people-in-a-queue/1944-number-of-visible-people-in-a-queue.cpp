891993ab96aefb0cc036a3f97c8df396034dd448
Sri Prasanna <24a95a0502@aec.edu.in>
2026-06-15 19:30:55 +0530

    Time: 21 ms (13.66%), Space: 88.7 MB (58.17%) - LeetHub

--- 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.cpp
+++ 1944-number-of-visible-people-in-a-queue/1944-number-of-visible-people-in-a-queue.cpp
@@ -2,21 +2,15 @@ class Solution {
 public:
     vector<int> canSeePersonsCount(vector<int>& h) {
         stack<int> st;
-        vector<int> ans(h.size(),0);
-        st.push(h[h.size()-1]);
-        for(int i=h.size()-2; i>=0; i--){
-            if(h[i]<st.top()){
-                ans[i]=1;
-                st.push(h[i]);
+        vector<int> ans(h.size(), 0);
+        for (int i = h.size() - 1; i >= 0; i--) {
+            while (!st.empty() && st.top() < h[i]) {
+                ans[i]++;
+                st.pop();
             }
-            else{
-                while(!st.empty() && st.top()<h[i]){
-                    ans[i]++;
-                    st.pop();
-                }
-                if(!st.empty() && st.top()>h[i]) ans[i]++;
-                st.push(h[i]);
-            }
+            if (!st.empty() && st.top() > h[i])
+                ans[i]++;
+            st.push(h[i]);
         }
         return ans;
     }
