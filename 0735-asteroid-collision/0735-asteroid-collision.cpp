68a16fdb7e7e54fcfe8df0a699b8edd42af540ba
Sri Prasanna <24a95a0502@aec.edu.in>
2026-06-15 21:07:36 +0530

    Time: 0 ms (100%), Space: 22.2 MB (17.99%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -2,26 +2,24 @@ class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& a) {
         stack<int> st;
-        vector<int> ans;
-        for(int i=a.size()-1; i>=0; i--){
-            int flag=0;
-            if(st.empty()){
+        for(int i=a.size()-1;i>=0;i--){
+            if(!st.empty() && a[i]>0 && st.top()<0){
+                while(!st.empty() && st.top()<0 && abs(st.top())<a[i]){
+                    st.pop();
+                }
+                if(!st.empty() && st.top()<0 && abs(st.top())==a[i]){
+                    st.pop();
+                    continue;
+                }
+                else if(!st.empty() && st.top()<0 && abs(st.top())>a[i]){
+                    continue;
+                }
                 st.push(a[i]);
             }
-            else{
-                if(a[i]>0 && st.top()<0){
-                    if(!st.empty() && abs(st.top())<a[i]){
-                        st.pop();
-                        i++;
-                    }
-                    else if(!st.empty() && abs(st.top())==a[i]){
-                        st.pop();
-                    }
-                }
-                else
-                    st.push(a[i]);
-            }
+            else
+            st.push(a[i]);
         }
+        vector<int> ans;
         while(!st.empty()){
             ans.push_back(st.top());
             st.pop();
