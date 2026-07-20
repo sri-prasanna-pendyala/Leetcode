cff804829e24f76124f8677bf7d8a4f685b05080
Sri Prasanna <24a95a0502@aec.edu.in>
2026-07-20 23:45:19 +0530

    Time: 0 ms (100%), Space: 8.5 MB (87.47%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int low=1,high=x,mid;
+        while(low<=high){
+            mid=low+(high-low)/2;
+            if(mid==x/mid)
+                return mid;
+            else if(mid>x/mid)
+                high=mid-1;
+            else
+                low=mid+1;
+        }
+        return high;
+    }
+};
