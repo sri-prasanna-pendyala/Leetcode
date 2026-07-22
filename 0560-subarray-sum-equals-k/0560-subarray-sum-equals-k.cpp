02f2cf74651a7ca0f1b81ca329950054f99a9c4b
Sri Prasanna <24a95a0502@aec.edu.in>
2026-07-22 15:46:31 +0530

    Time: 34 ms (89.15%), Space: 45.6 MB (40.82%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int presum=0,cnt=0;
-        unordered_map<int,int> freq;
-        freq[0]=1;
+        unordered_map<int,int> mp;
+        mp[0]=1;
+        int sum=0,cnt=0;
         for(int i=0;i<nums.size();i++){
-            presum+=nums[i];
-            if(freq.find(presum-k)!=freq.end()){
-                cnt+=freq[presum-k];
+            sum+=nums[i];
+            if(mp.find(sum-k)!=mp.end()){
+                cnt+=mp[sum-k];
             }
-            freq[presum]++;
+            mp[sum]++;
         }
         return cnt;
     }
