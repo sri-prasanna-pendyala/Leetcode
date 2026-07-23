db124d2099fa328a550a98f92542de4591f5994c
Sri Prasanna <24a95a0502@aec.edu.in>
2026-07-23 13:55:10 +0530

    Time: 0 ms (100%), Space: 22.7 MB (53.74%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int l=0,r=0,k=1;
+        int l=0,r=1;
         while(r<nums.size()){
-            if(nums[l]==nums[r])
-                r++;
-            else{
-                swap(nums[++l],nums[r++]);
-                k++;
+            if(nums[l]!=nums[r]){
+                l++;
+                swap(nums[l],nums[r]);
             }
+            r++;
         }
-        return k;
+        return l+1;
     }
-
 };
